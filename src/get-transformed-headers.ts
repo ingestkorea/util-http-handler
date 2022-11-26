0798b47b0fa0135045623a7495448599b722b64b
swhan <swhan@ingestkorea.com>
2022-11-26 17:33:00 +0900

    remove @types/node dependency

--- src/get-transformed-headers.ts
+++ src/get-transformed-headers.ts
@@ -3,12 +3,11 @@ import { IncomingHttpHeaders } from "http";
 
 export const getTransformedHeaders = (headers: IncomingHttpHeaders): HeaderBag => {
     const init: HeaderBag = {};
-    const transformedHeaders = Object.keys(headers).reduce((acc, curr)=>{
-        const headerName = curr;
-        const headerValues = headers[headerName];
-        if(!headerValues) return acc;
-        
-        acc[headerName] = Array.isArray(headerValues) ? headerValues.join(",") : headerValues
+    const transformedHeaders = Object.keys(headers).reduce((acc, name) => {
+        const headerValues = headers[name];
+        if (!headerValues) return acc;
+
+        acc[name] = Array.isArray(headerValues) ? headerValues.join(",") : headerValues
         return acc;
     }, init);
     return transformedHeaders;
